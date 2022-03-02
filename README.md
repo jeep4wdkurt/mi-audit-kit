11ce6f8f59047333f724396850daa5ac2805d436
Kurt Schulte <jeep4wdkurt@gmail.com>
2022-03-02 13:57:37 -0500

    Software kit 01.00.304 publish at 2022.03.02.1357

--- README.md
+++ README.md
@@ -12,5 +12,5 @@ No use without written authorization.
 ## History
 ```
     Date        Version    Author           Desc
-    2022.02.25  01.00.303  F. Kurt Schulte  Original Version
+    2022.03.02  01.00.304  F. Kurt Schulte  Original Version
 ```
--- core-install.sh
+++ core-install.sh
@@ -17,15 +17,14 @@
 ####################################################################################################
 
 #
-# Dataset Encryption Salt generation
+# MAUDE Dataset Encryption Salt generation
 #
 datasetSaltGenerate() {
 	local saltPass="$1"
 	local saltLength=128
+	progLabel="${datasetSaltGenerate}" ; barfts "${progLabel}.Entry(saltLength=${saltLength},saltFile='${datasetSaltFile}')..."
 
-	barfds "MAUDE Dataset Salt Generate (saltLength=${saltLength},saltFile='${datasetSaltFile}')..."
-
-	[ "${saltPass}" == "" ] && barfe "datasetSaltGenerate.Error: Pass phrase is required."
+	[ "${saltPass}" == "" ] && barfee "Pass phrase is required."
 
 	# Make Dataset Salt
 	head -c $(( $saltLength * 4 )) /dev/random |
@@ -34,47 +33,51 @@ datasetSaltGenerate() {
 		head -c $saltLength |
 		$gpgCommand --batch --symmetric --passphrase "${saltPass}" >"${datasetSaltFile}"
 
-	errCode=$? ; [ $errCode -ne 0 ] && barfe "datasetSaltGenerate.Error: Problem generating dataset salt file '${datasetSaltFile}'"
+	errCode=$? ; [ $errCode -ne 0 ] && barfee "Problem generating dataset salt file '${datasetSaltFile}'"
 
-	barfds "MAUDE Dataset Salt Generate complete."
+	barfvs "MAUDE Dataset Salt Generate complete."
+	
+	barfts "datasetSaltGenerate.Exit"
 }
 
 #
 # Get dataset encryption salt
 #
 datasetSaltFetch() {
-
 	local saltPass="$1"
+	progLabel="datasetSaltFetch"
+	barfts "${progLabel}.Entry()"
 
 	# Get Dataset Salt
-	[ ! -f "${datasetSaltFile}" ] && barfe "DatasetPublish.SaltGet.Error: Can't find salt file '${datasetSaltFile}'"
+	[ ! -f "${datasetSaltFile}" ] && barfee "Can't find salt file '${datasetSaltFile}'"
 	datasetSalt=$($gpgCommand --quiet --batch --decrypt --output - --passphrase "${saltPass}" "${datasetSaltFile}")
 	errCode=$? 
 	if [ $errCode -ne 0 ] || [ "${datasetSalt}" == "" ] ; then
-		barfe "datasetSaltFetch.SaltGet.BadPass: Bad pass phrase for salt."
+		barfee "Bad pass phrase for salt."
 	fi
 	#barfd "datasetSalt : ${datasetSalt}"
-
+	barfts "datasetSaltFetch.Exit"
 }
 
 #
 # Pull latest MAUDE Software project from github
 #
 softwareProjectPull() {
-
+	progLabel="softwareProjectPull" ; barfts "${progLabel}.Entry()"
+	
 	softwarePullStatus=1	; export softwarePullStatus
 
 	if [ ! -d "${maudSoftwareKitFolder}" ] ; then
 		# Clone MAUDE Software kit project
 		[ ! -d "${maudKitsRoot}" ] &&
-			barfe "MAUDE.SoftwareProjectPull.ConfigurationError: Can't find kitsRoot folder '${maudKitsRoot}', aborting."
+			barfee "Configuration problem - Can't find [maudKitsRoot] folder '${maudKitsRoot}', aborting."
 		cd "${maudKitsRoot}"
 		
 		barfvs "MAUDE Software project coning..."
 		git clone "${maudSoftwareKitProject}"
 		errorCode=$?
 		[ $errorCode -eq 0 ] && { cd "${maudSoftwareKitFolder}" ; git config pull.rebase false ; }
-		[ $errorCode -ne 0 ] && barfe "MAUDE.SoftwareProjectPull.CloneError: failed to clone project '${maudSoftwareKitProject}'"
+		[ $errorCode -ne 0 ] && barfee "Failed to clone MAUDE softare project '${maudSoftwareKitProject}'"
 		barfvs "MAUDE.SoftwareProjectPull. MAUDE Software project cloning complete."
 	else
 		# Pull latest version of MAUDE Software kit
@@ -83,31 +86,34 @@ softwareProjectPull() {
 		barfvs "MAUDE Software pulling latest version of project..."
 		git pull origin main
 		errorCode=$?
- 		[ $errorCode -ne 0 ] && barfe "MAUDE.SoftwareProjectPull.PullError: failed to pull latest version of project"
+ 		[ $errorCode -ne 0 ] && barfee "Failed to pull latest version of MAUDE software project"
 		barfvs "MAUDE Software pulling latest version of project complete."
 	fi
 	
 	softwarePullStatus=0	; export softwarePullStatus
+	
+	barfts "softwareProjectPull.Exit(softwarePullStatus=${softwarePullStatus})"
 }
 
 #
 # Pull latest MAUDE Reference data project from github
 #
 referenceDataProjectPull() {
+	progLabel="referenceDataProjectPull" ; barfts "${progLabel}.Entry()"
 
 	referencePullStatus=1	; export referencePullStatus
 
 	if [ ! -d "${maudDataKitFolder}" ] ; then
 		# Clone MAUDE Reference Data kit project
 		[ ! -d "${maudKitsRoot}" ] &&
-			barfe "MAUDE.ReferenceDataProjectPull.ConfigurationError: Can't find kitsRoot folder '${maudKitsRoot}', aborting."
+			barfee "Configuration problem - Can't find [maudKitsRoot] folder '${maudKitsRoot}', aborting."
 		cd "${maudKitsRoot}"
 		
 		barfvs "MAUDE Reference Data cloning reference data project..."
 		git clone "${maudDataKitProject}"
 		errorCode=$?
 		[ $errorCode -eq 0 ] && { cd "${maudDataKitFolder}"; git config pull.rebase false ; errCode=$? ; }
-		[ $errorCode -ne 0 ] && barfe "MAUDE.ReferenceDataProjectPull.CloneError: failed to clone project '${maudDataKitProject}'"
+		[ $errorCode -ne 0 ] && barfee "Failed to clone MAUDE Reference Data project '${maudDataKitProject}'"
 		barfvs "MAUDE.ReferenceDataProjectPull: MAUDE Data install cloning reference data project complete."
 	else
 		# Pull latest version of MAUDE Reference Data kit
@@ -116,28 +122,33 @@ referenceDataProjectPull() {
 		barfvs "MAUDE Reference Data pulling latest version of reference project..."
 		git pull origin main
 		errorCode=$?
- 		[ $errorCode -ne 0 ] && barfe "MAUDE.ReferenceDataProjectPull.PullError: failed to pull latest version of project"
+ 		[ $errorCode -ne 0 ] && barfee "Failed to pull latest version of MAUDE Reference Data project"
 		barfvs "MAUDE Reference Data pulling latest version of reference data project complete."
 	fi
 
 	barfds "HERE1"
 	referencePullStatus=0	; export referencePullStatus
+	
+	barfts "referenceDataProjectPull.Exit(referencePullStatus=${referencePullStatus})"
 }
 
 #
 # Verify UberGen is installed
 #
 ubergenProjectVerifyInstalled() { 
+	progLabel="ubergenProjectVerifyInstalled" ; barfts "${progLabel}.Entry()"
 
 	ubergenIsInstalled="N"	; export ubergenIsInstalled
 
-	[ "${ubergenRoot}" == "" ]			&& barfe "ubergenProjectVerifyInstalled.Error: ubergenRoot is not defined"
-	[ "${ubergenFolder}" == "" ]		&& barfe "ubergenProjectVerifyInstalled.Error: ubergenFolder is not defined"
-	[ "${ubergenKitProject}" == "" ]	&& barfe "ubergenProjectVerifyInstalled.Error: ubergenKitProject is not defined"
+	[ "${ubergenRoot}" == "" ]			&& barfee "[ubergenRoot] is not defined"
+	[ "${ubergenFolder}" == "" ]		&& barfee "[ubergenFolder] is not defined"
+	[ "${ubergenKitProject}" == "" ]	&& barfee "[ubergenKitProject] is not defined"
 
 	if [ -d "${ubergenFolder}" ] ;then
 		ubergenIsInstalled="Y"	; export ubergenIsInstalled
 	fi
+	
+	barfts "ubergenProjectVerifyInstalled.Exit(ubergenIsInstalled=${ubergenIsInstalled})"
 }
 
 
@@ -145,13 +156,14 @@ ubergenProjectVerifyInstalled() {
 # Pull latest UberGen project from github
 #
 ubergenProjectPull() {
-
+	progLabel="ubergenProjectPull" ; barfts "${progLabel}.Entry()"
+	
 	ubergenPullStatus=1	; export ubergenPullStatus
 
-	[ "${maudFolderPerms}" == "" ]		&& barfe "ubergenProjectPull.Error: maudFolderPerms is not defined"
-	[ "${ubergenRoot}" == "" ]			&& barfe "ubergenProjectPull.Error: ubergenRoot is not defined"
-	[ "${ubergenFolder}" == "" ]		&& barfe "ubergenProjectPull.Error: ubergenFolder is not defined"
-	[ "${ubergenKitProject}" == "" ]	&& barfe "ubergenProjectPull.Error: ubergenKitProject is not defined"
+	[ "${maudFolderPerms}" == "" ]		&& barfee "[maudFolderPerms] is not defined"
+	[ "${ubergenRoot}" == "" ]			&& barfee "[ubergenRoot] is not defined"
+	[ "${ubergenFolder}" == "" ]		&& barfee "[ubergenFolder] is not defined"
+	[ "${ubergenKitProject}" == "" ]	&& barfee "[ubergenKitProject] is not defined"
 
 	# Get UberGen Key Code
 	ubergenKeycodeDecode "${optPass}"
@@ -159,9 +171,9 @@ ubergenProjectPull() {
 	barfds "DEBUG: UberGen Keycode Decode : ${ubergenKeycodeStatus}"
 	#barfds "DEBUG: UberGen Keycode        : ${ubergenKeycode}"
 	if [ "${ubergenKeycodeStatus}" == "" ] || [ $ubergenKeycodeStatus -ne 0 ] ; then
-		barfe "PROBLEM decrypting UberGen pass code"
+		barfee "Problem decrypting UberGen pass code"
 	fi
-	[ "${ubergenKeycode}" == "" ] && barfe "HUGE PROBLEM: UberGen key code decrypted to blank"
+	[ "${ubergenKeycode}" == "" ] && barfee "HUGE PROBLEM - UberGen key code decrypted to blank"
 
 	# Get project
 	local unpackNeeded=1
@@ -169,14 +181,14 @@ ubergenProjectPull() {
 	if [ ! -d "${ubergenFolder}" ] ; then
 		# Clone UberGen kit project
 		[ ! -d "${ubergenRoot}" ] &&
-			barfe "UberGenProjectPull.ConfigurationError: Can't find ubergenRoot folder '${ubergenRoot}', aborting."
+			barfee "Can't find [ubergenRoot] folder '${ubergenRoot}', aborting."
 		cd "${ubergenRoot}"
 		
 		barfs "UberGen project cloning ..."
 		git clone "${ubergenKitProject}"
 		errorCode=$?
 		[ $errorCode -eq 0 ] && { cd "${ubergenFolder}" ; git config pull.rebase false ; }
-		[ $errorCode -ne 0 ] && barfe "UberGenProjectPull.CloneError: failed to clone project '${ubergenKitProject}'"
+		[ $errorCode -ne 0 ] && barfee "Failed to clone UberGen project '${ubergenKitProject}'"
 		barfs "UberGen project cloning complete."
 	else
 		# Create user temp folder if not already there
@@ -187,7 +199,7 @@ ubergenProjectPull() {
 		local gitTempLogFile="${tempFolder}/git.log"		
 		cd "${ubergenFolder}"
 		git pull origin main >"${gitTempLogFile}"
-		errorCode=$? ;  [ $errorCode -ne 0 ] && barfe "UberGenProjectPull.PullError: failed to pull latest version of project"
+		errorCode=$? ;  [ $errorCode -ne 0 ] && barfee "Failed to pull latest version of UberGen project"
 
 		# Determine if code was already up to date, or whether code pulled and needs an unpack
 		uberAlreayUpdated=$(cat "${gitTempLogFile}" | grep -c 'Already up to date')
@@ -213,26 +225,26 @@ ubergenProjectPull() {
 		./ubergen.sh -u -P "${ubergenKeycode}" $logFlag
 		errCode=$?
 		barfs "UberGen unpacking project complete."
-		[ $errorCode -ne 0 ] && barfe "UberGenProjectPull.UnpackError: failed to unpack latest version of project"
+		[ $errorCode -ne 0 ] && barfee "Failed to unpack latest version of UberGen project"
 	fi
 	
 	ubergenPullStatus=0	; export ubergenPullStatus
+
+	barfts "ubergenProjectPull.Exit(ubergenPullStatus=${ubergenPullStatus})"
 }
 
 #
 # Uninstall Ubergen Project, possibly based on version number
 #
 ubergenProjectUninstall() {
-
 	local minVersion="$1"
-
-	barfds "ubergenProjectUninstall.Entry(minVersion=${minVersion})"
+	progLabel="ubergenProjectUninstall" ; barfts "${progLabel}.Entry(minVersion=${minVersion})"
 
 	ubergenUninstallStatus=1	; export ubergenUninstallStatus
 
-	[ "${ubergenRoot}" == "" ]				&& barfe "ubergenProjectUninstall.Error: ubergenRoot is not defined"
-	[ "${ubergenFolder}" == "" ]			&& barfe "ubergenProjectUninstall.Error: ubergenFolder is not defined"
-	[ "${ubergenVersionFile}" == "" ]		&& barfe "ubergenProjectUninstall.Error: ubergenVersionFile is not defined"
+	[ "${ubergenRoot}" == "" ]				&& barfee "[ubergenRoot] is not defined"
+	[ "${ubergenFolder}" == "" ]			&& barfee "[ubergenFolder] is not defined"
+	[ "${ubergenVersionFile}" == "" ]		&& barfee "[ubergenVersionFile] is not defined"
 
 	if [ -d "${ubergenFolder}" ] ;then
 
@@ -241,30 +253,30 @@ ubergenProjectUninstall() {
 		if [ "${minVersion}" != "" ] ; then
 			installedVersion=$(cat "${ubergenVersionFile}" | grep 'UBERGEN_VERSION' | sed -e 's~^.*=~~;s~#.*~~;s~ \+$~~' )
 			errCode=$? ; [ $errCode -ne 0 ] &&
-				barfe "ubergenProjectUninstall.Error:Can't read UberGen version file '${ubergenVersionFile}'"
+				barfee "Can't read UberGen version file '${ubergenVersionFile}'"
 			[ "${installedVersion}" == "" ] && installedVersion=01.00
 
 			[ $(echo "${installedVersion}" | grep -c '^[0-9][0-9][.][0-9][0-9]$') -eq 0 ] &&
-				barfe "ubergenProjectUninstall.Error:Bad installed version number format in UberGen version file '${ubergenVersionFile}', version='${installedVersion}'"
+				barfee "Bad installed version number format in UberGen version file '${ubergenVersionFile}', version='${installedVersion}'"
 				
 			versionCompare=$(perl -e "print (( ${installedVersion} < ${minVersion} ) ? "'"1":"0"); ')
-			errCode=$? ; [ $errCode -ne 0 ] && barfe "ubergenProjectUninstall.Error:Can't test UberGen version. Code Error."
+			errCode=$? ; [ $errCode -ne 0 ] && barfee "Can't test UberGen version. Code Error."
 			[ "${versionCompare}" == "0" ] && needUninstall=0
 		fi
 		
 		# Uninstall UberGen
 		if [ $needUninstall -ne 0 ] ; then
 			find "${ubergenFolder}" -type f -printf '"%p"\n' | xargs -L1 rm $verboseFlag
-			errCode=$? ; [ $errCode -ne 0 ] && barfe "ubergenProjectUninstall.Error:Can't remove UberGen files(s)."
+			errCode=$? ; [ $errCode -ne 0 ] && barfee "Can't remove UberGen files(s)."
 
 			find "${ubergenFolder}" -type d -printf '"%p"\n' | sort -r | xargs -L1 rmdir $verboseFlag
-			errCode=$? ; [ $errCode -ne 0 ] && barfe "ubergenProjectUninstall.Error:Can't remove UberGen folder(s)."
+			errCode=$? ; [ $errCode -ne 0 ] && barfee "Can't remove UberGen folder(s)."
 		fi
 	fi
 	
 	ubergenUninstallStatus=0	; export ubergenUninstallStatus
 	
-	barfds "ubergenProjectUninstall.Exit(ubergenUninstallStatus=${ubergenUninstallStatus})"
+	barfts "ubergenProjectUninstall.Exit(ubergenUninstallStatus=${ubergenUninstallStatus})"
 }
 
 #
@@ -273,10 +285,10 @@ ubergenProjectUninstall() {
 ubergenKeycodeEncode() {
 	local keyPass="$1"
 	local keyFilespec="$2"
-	barfds "MAUDE UberGen Keycode Encode (keyCodeFile='${keyFilespec}')..."
+	progLabel="ubergenKeycodeEncode" ; barfts "${progLabel}.Entry(keyCodeFile='${keyFilespec}')"
 
-	[ "${keyPass}" == "" ]		&& barfe "ubergenKeycodeEncode.Error: keyPass may not be blank"
-	[ "${keyFilespec}" == "" ]	&& barfe "ubergenKeycodeEncode.Error: keyFilespec is not defined"
+	[ "${keyPass}" == "" ]		&& barfee "[keyPass] may not be blank"
+	[ "${keyFilespec}" == "" ]	&& barfee "[keyFilespec] is not defined"
 
 	ubergenKeycodeStatus=1 ; 		export ubergenKeycodeStatus
 
@@ -284,17 +296,19 @@ ubergenKeycodeEncode() {
 	local ubergenKeycode=
 	while [ "${ubergenKeycode}" == "" ] ; do
 		read -p "UberGen Key Code: " ubergenKeycode
-		errCode=$? ; [ $errCode -ne 0 ] && barfe "ubergenKeycodeEncode.InputError: Bad, bad user."
+		errCode=$? ; [ $errCode -ne 0 ] && barfee "Bad, bad user."
 	done
 
 	# Encode key code
 	echo "${ubergenKeycode}" |
 		$gpgCommand --batch --symmetric --passphrase "${keyPass}" >"${keyFilespec}"
-	errCode=$? ; [ $errCode -ne 0 ] && barfe "ubergenKeycodeEncode.EncryptError: Problem encrypting UberGen key code."
+	errCode=$? ; [ $errCode -ne 0 ] && barfee "Problem encrypting UberGen key code."
 
 	ubergenKeycodeStatus=0 ; 		export ubergenKeycodeStatus
 
 	barfds "MAUDE UberGen Keycode Encode complete."
+	
+	barfts "ubergenKeycodeEncode.Exit(ubergenKeycodeStatus=${ubergenKeycodeStatus})"
 }
 
 #
@@ -302,13 +316,13 @@ ubergenKeycodeEncode() {
 #
 ubergenKeycodeDecode() {
 	local keyPass="$1"
-	barfds "MAUDE UberGen Keycode Decode (keyCodeFile='${maudUbergenKeyFileSpec}')..."
+	progLabel="ubergenKeycodeDecode" ; barfts "${progLabel}.Entry(keyCodeFile='${maudUbergenKeyFileSpec}')"
 
 	ubergenKeycodeStatus=1 ; 		export ubergenKeycodeStatus
 	ubergenKeycode="<invalid>" ;	export ubergenKeycode
 	
-	[ "${keyPass}" == "" ]					&& barfe "ubergenKeycodeDecode.Error: keyPass may not be blank"
-	[ "${maudUbergenKeyFileSpec}" == "" ]	&& barfe "ubergenKeycodeDecode.Error: maudUbergenKeyFileSpec is not defined"
+	[ "${keyPass}" == "" ]					&& barfee "[keyPass] may not be blank"
+	[ "${maudUbergenKeyFileSpec}" == "" ]	&& barfee "[maudUbergenKeyFileSpec] is not defined"
 
 	# decode key code
 	ubergenKeycode=$(cat "${maudUbergenKeyFileSpec}" |
@@ -319,4 +333,6 @@ ubergenKeycodeDecode() {
 	ubergenKeycodeStatus=0 ; export ubergenKeycodeStatus;
 
 	barfds "MAUDE UberGen Keycode Decode complete."
+	
+	barfts "ubergenKeycodeDecode.Exit(ubergenKeycodeStatus=${ubergenKeycodeStatus})"
 }
